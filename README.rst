8b9a35b04e898e959795f80fd51e12a7562962ea
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-11-24 12:10:00 +0200

    Added testing integration to Travis CI.

--- README.rst
+++ README.rst
@@ -5,10 +5,11 @@ santa.theme
 Change log
 ----------
 
-0.3 (2012-11-23)
+0.3 (2012-11-xx)
 ================
 
 - Updated styles. [taito]
+- Added testing integration to Travis CI. [taito]
 
 0.2 (2012-10-30)
 ================
--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ setup(
     version='0.3',
     description="Theme for Santa site.",
     long_description=open("README.rst").read(),
-    # Get more strings from
-    # http://pypi.python.org/pypi?:action=list_classifiers
     classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 4.2",
@@ -19,11 +17,14 @@ setup(
     author_email='taito.horiuchi@abita.fi',
     url='http://santa.abita.fi/',
     license='Non-free',
-    packages=find_packages(exclude=['ez_setup']),
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
     namespace_packages=['santa'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'Products.CMFPlone>=4.2',
+        'five.pt',
         'hexagonit.testing',
         'plone.app.theming',
         'plone.app.themingplugins',
