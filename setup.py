0c8244c02cdd7b03f8f0f4c3f85d179812dee264
Taito Horiuchi <taito.horiuchi@gmail.com>
2013-03-11 22:36:57 +0200

    Release 0.5.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='santa.theme',
-    version='0.5.2',
+    version='0.5.3',
     description="Theme for Santa site.",
     long_description=open("README.rst").read(),
     classifiers=[
